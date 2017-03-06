cbcacd58d645545525df14a4e3e1c15c91e31302
Peter DeMartini <thepeterdemartini@gmail.com>
2017-03-06 11:33:36 -0700

    configure-logging

---
+++ configure-logging.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+##
+
+usage(){
+  echo 'USAGE: configure-logging <logentries-token>'
+  echo ''
+  echo 'Arguments:'
+  echo '  -h, --help         print this help text'
+  echo '  -v, --version      print the version'
+  echo ''
+}
+
+version() {
+  echo '1.0.0'
+}
+
+assert_logentries_token() {
+  local logentries_token="$1"
+  if [ -z "$logentries_token" ]; then
+    echo "Missing logentries token"
+    exit 1
+  fi
+}
+
+create_octoswarm_dir() {
+  mkdir -p /run/octoswarm
+}
+
+write_logentries_script() {
+  local logentries_token="$1"
+  echo "#!/bin/bash
+journalctl --utc --follow --output=short \\
+  | awk -v token=$logentries_token '{ print token, \$0; fflush(); }' \\
+  | ncat --ssl --ssl-verify data.logentries.com 20000" | tee /run/octoswarm/log-to-logentries.sh
+}
+
+make_executable() {
+  chmod +x /run/octoswarm/logentries_token.sh
+}
+
+write_unit_file() {
+  echo '[Unit]
+Description=Push journal logs to logentries.com
+After=systemd-journald.service
+After=systemd-networkd.service
+[Service]
+Restart=always
+ExecStart=/bin/bash /run/octoswarm/log-to-logentries.sh
+[Install]
+WantedBy=multi-user.target' | tee /run/octoswarm/log-to-logentries.service
+}
+
+start_unit() {
+  systemctl start /run/octoswarm/log-to-logentries.service
+}
+
+enable_unit() {
+  systemctl enable log-to-logentries.service
+}
+
+main() {
+  local logentries_token="$1"
+  case "$logentries_token" in
+    -h | --help | help)
+      usage
+      exit 0
+      ;;
+    -v | --version | version)
+      version
+      exit 0
+      ;;
+  esac
+  assert_logentries_token "$logentries_token"
+  create_octoswarm_dir \
+  && write_logentries_script "$logentries_token" \
+  && make_executable \
+  && write_unit_file \
+  && start_unit \
+  && enable_unit
+}
+
+main "$@"
